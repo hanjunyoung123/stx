3bc112bad92ed7b896355c5e698ed435920d518b
hanjunyoung <hjy961017@gamil.com>
2024-07-01 12:57:01 +0900

    if else 조건문 addclass붙여서 css에 추가적용

--- js/main.js
+++ js/main.js
@@ -14,5 +14,35 @@ window.onload = function(){
         if(section){
             section.scrollIntoView({ behavior: "smooth" });
         }
-    }
+    }    
+    // top버튼 스크롤 기능====================================
+const topbtn = document.getElementById("top-btn")
+topbtn.addEventListener("click",function(event){
+event.preventDefault()
+// console.log(window.scrollY);
+if(window.scrollY == 0){
+window.scrollTo({
+    top:2496,
+    behavior: "smooth"
+});
+}
+else{
+window.scrollTo({
+    top:0,
+    behavior: "smooth"
+})
+}
+})
+// 화살표 이미지 회전
+const topBtnImg = document.getElementById("top-btn-img")
+window.addEventListener("scroll",function(scTop){
+scTop = window.document.documentElement.scrollTop;
+if(scTop > 0){
+    topBtnImg.classList.add("up")
+}
+else{
+    topBtnImg.classList.remove("up")
+
+}
+});
 };
